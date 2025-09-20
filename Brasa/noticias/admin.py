33614da0ff421f208f2582fd1645a24c9d4706da
Bruno Augusto <bruno.filho1000@gmail.com>
2025-09-20 09:56:20 -0300

    .

--- Brasa/noticias/admin.py
+++ Brasa/noticias/admin.py
@@ -6,9 +6,3 @@ class NoticiaAdmin(admin.ModelAdmin):
     list_display  = ('titulo', 'autor', 'categoria', 'data_publicacao', 'destaque')
     list_filter   = ('categoria', 'destaque', 'data_publicacao')
     search_fields = ('titulo', 'conteudo', 'autor')
-
-@admin.register(Voto)
-class VotoAdmin(admin.ModelAdmin):
-    list_display  = ('noticia', 'usuario', 'valor', 'atualizado_em')
-    list_filter   = ('valor', 'atualizado_em')
-    search_fields = ('noticia__titulo', 'usuario__username')
