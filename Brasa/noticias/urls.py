c041389e014ab6e8daa3aba8cbddaa2bdb89eaa1
Maria Augusta Hatem da Fonte <mariaaugustahdafonte@gmail.com>
2025-09-22 16:34:41 -0300

    Update urls.py

--- Brasa/noticias/urls.py
+++ Brasa/noticias/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('salvar/<int:noticia_id>/', views.toggle_salvar_noticia, name='toggle_salvar'),
+    path('salvar/<int:noticia_id>/', views.botao_salvar_noticia, name='toggle_salvar'),
     path('salvos/', views.noticias_salvas, name='noticias_salvas'),
 ]
