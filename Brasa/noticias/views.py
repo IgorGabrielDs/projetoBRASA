360b65595c91e17d94f017eb398e930b5796196d
Maria Augusta Hatem da Fonte <mariaaugustahdafonte@gmail.com>
2025-09-22 16:34:14 -0300

    Update views.py
    add salvar_noticia

--- Brasa/noticias/views.py
+++ Brasa/noticias/views.py
@@ -4,6 +4,41 @@ from django.http import JsonResponse
 from django.contrib import messages
 from .models import Noticia, Voto
 
+def home(request):
+    noticias = Noticia.objects.order_by('-data_publicacao')
+    
+    noticias_salvas_ids = []
+    if request.user.is_authenticated:
+        noticias_salvas_ids = request.user.noticias_salvas.values_list('id', flat=True)
+
+    contexto = {
+        'noticias': noticias,   
+        'noticias_salvas_ids': noticias_salvas_ids,
+    }
+    return render(request, 'portal/home.html', contexto)
+
+@login_required
+def noticias_salvas(request):
+    noticias = request.user.noticias_salvas.order_by('-data_publicacao')
+    contexto = {
+        'noticias': noticias,
+    }
+    return render(request, 'portal/noticias_salvas.html', contexto)
+
+@login_required
+def botao_salvar_noticia(request, noticia_id):
+    noticia = get_object_or_404(Noticia, id=noticia_id)
+    
+    if noticia in request.user.noticias_salvas.all():
+        
+        request.user.noticias_salvas.remove(noticia)
+        salvo = False
+    else:
+        request.user.noticias_salvas.add(noticia)
+        salvo = True
+        
+    return JsonResponse({'salvo': salvo})
+
 def noticia_detalhe(request, pk):
     noticia = get_object_or_404(Noticia, pk=pk)
     voto_usuario = None
@@ -52,5 +87,6 @@ def votar(request, pk):
             'down': noticia.downvotes(),
             'meu_voto': 0 if status == 'removido' else valor
         })
+
 
     return redirect('noticia_detalhe', pk=pk)
