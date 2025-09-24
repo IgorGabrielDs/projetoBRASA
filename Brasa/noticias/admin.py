9e409ce91e3c4de2b8e649b7db159b5fee4fe816
Gustavo Torres Castro <gustavotorrescastro@gmail.com>
2025-09-24 12:52:24 -0300

    Delete Brasa directory

--- Brasa/noticias/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Noticia
-
-@admin.register(Noticia)
-class NoticiaAdmin(admin.ModelAdmin):
-    list_display  = ('titulo', 'autor', 'categoria', 'data_publicacao', 'destaque')
-    list_filter   = ('categoria', 'destaque', 'data_publicacao')
-    search_fields = ('titulo', 'conteudo', 'autor')
-
--- Brasa/noticias/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('salvar/<int:noticia_id>/', views.botao_salvar_noticia, name='toggle_salvar'),
-    path('salvos/', views.noticias_salvas, name='noticias_salvas'),
-]
