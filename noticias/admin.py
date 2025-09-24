fe9be11f58213de76f3ec7758d8c39ba380de598
Bruno Augusto <bruno.filho1000@gmail.com>
2025-09-24 15:01:35 -0300

    Update admin.py
    Atualização para colocar imagem nas notícias pelo admin

--- noticias/admin.py
+++ noticias/admin.py
@@ -3,11 +3,14 @@ from .models import Noticia, Voto
 
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
-    list_display  = ('titulo', 'autor', 'categoria', 'data_publicacao', 'destaque')
-    list_filter   = ('categoria', 'destaque', 'data_publicacao')
-    search_fields = ('titulo', 'conteudo', 'autor')
+    list_display = ("id","titulo","criado_em")
+    search_fields = ("titulo","conteudo")
+    ordering = ("-criado_em",)
+    fields = ("titulo","conteudo","imagem","legenda","criado_em")
+    readonly_fields = ("criado_em",)
 @admin.register(Voto)
 class VotoAdmin(admin.ModelAdmin):
     list_display = ("id","noticia","usuario","valor")
     list_filter = ("valor",)
+
 
