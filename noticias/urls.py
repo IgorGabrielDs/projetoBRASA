465ef66575fbdec0067167676bfc0c75e7dc0cc6
Bruno Augusto <bruno.filho1000@gmail.com>
2025-10-12 20:44:09 -0300

    Update urls.py
    Volta

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,15 +1,14 @@
-# noticias/urls.py
 from django.urls import path
 from . import views
 
-app_name = "noticias"
+app_name = 'noticias'
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("noticia/<int:pk>/", views.noticia_detalhe, name="noticia_detalhe"),
-    path("noticia/<int:pk>/votar/", views.votar, name="votar"),
-    path("noticia/<int:pk>/toggle-salvo/", views.toggle_salvo, name="toggle_salvo"),
-    path("minhas-salvas/", views.minhas_salvas, name="minhas_salvas"),
-    path("signup/", views.signup, name="signup"),
-    path("noticia/<int:pk>/resumir/", views.resumir_noticia, name="resumir_noticia"),
+    path('', views.index, name='index'),
+    path('noticia/<int:pk>/', views.noticia_detalhe, name='noticia_detalhe'),
+    path('noticia/<int:pk>/votar/', views.votar, name='votar'),
+    path('accounts/signup/', views.signup, name='signup'),  
+    path("salvos/", views.minhas_salvas, name="salvos"),
+    path("noticia/<int:pk>/salvar/", views.toggle_salvo, name="toggle_salvo"),
+    path('noticia/<int:pk>/resumir/', views.resumir_noticia, name='resumir_noticia'),
 ]
