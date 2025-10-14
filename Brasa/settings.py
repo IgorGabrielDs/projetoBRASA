d6d838c33f84fbef1d02e349749c5a18f3cd085e
Gustavotorrescastro <gustavotorrescastro@gmail.com>
2025-10-14 10:43:04 -0300

    Atualizando Repositório

--- Brasa/settings.py
+++ Brasa/settings.py
@@ -102,9 +102,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'noticias/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
-STATICFILES_DIRS = [BASE_DIR / 'noticias/static']
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'noticias/static',
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
