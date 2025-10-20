85190211062a666dae93e73514eb8523993a7fa2
Gustavotorrescastro <gustavotorrescastro@gmail.com>
2025-10-20 14:00:08 -0300

    Atualizando Repositório

--- Brasa/settings.py
+++ Brasa/settings.py
@@ -14,6 +14,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'projetobrasa.azurewebsites.net']
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 
+# Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
