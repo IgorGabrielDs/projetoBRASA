807bdee9f99524aed48905f304b8219f09b9f1b0
Gustavotorrescastro <gustavotorrescastro@gmail.com>
2025-10-23 11:04:32 -0300

    Ajeitando login

--- Brasa/urls.py
+++ Brasa/urls.py
@@ -1,7 +1,3 @@
-# ======================================================
-# BRASA — urls.py (estrutura padronizada estilo JC)
-# ======================================================
-
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -9,17 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="registration/login.html"),
-        name="login",
-    ),
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(template_name="registration/logged_out.html"),
-        name="logout",
-    ),
-
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name="logout"),
     path("admin/", admin.site.urls),
     path("", include(("noticias.urls", "noticias"), namespace="noticias")),
 ]
