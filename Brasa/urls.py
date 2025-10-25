5d45558b91822db747c1122fd52109aec9b7a769
Gustavotorrescastro <gustavotorrescastro@gmail.com>
2025-10-25 11:28:58 -0300

    Tentativa de ajeitar a aba 'Login'

--- Brasa/urls.py
+++ Brasa/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name="logout"),
     path("admin/", admin.site.urls),
     path("", include(("noticias.urls", "noticias"), namespace="noticias")),
     path('accounts/', include('django.contrib.auth.urls')),
